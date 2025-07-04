2e5a8c6ada82f78fbd157f5144aca42cde6b63fd
tomaslanterna <tomaslanterna@gmail.com>
2025-07-04 19:34:51 -0300

    fix: add service url to env

--- middlewares/verifyTwiloSignatureMiddleware.js
+++ middlewares/verifyTwiloSignatureMiddleware.js
@@ -2,13 +2,12 @@ const twilio = require("twilio");
 
 const verifyTwilioSignature = (req, res, next) => {
   const signature = req.headers["x-twilio-signature"];
-  const { TWILIO_AUTH_TOKEN } = process.env;
+  const { TWILIO_AUTH_TOKEN, SERVICE_URL } = process.env;
 
-  const url = `https://9f9f-2800-a4-1d40-5d00-d811-ce27-abc6-6c3a.ngrok-free.app/message/whatsapp/webhook`;
   const isValid = twilio.validateRequest(
     TWILIO_AUTH_TOKEN,
     signature,
-    url,
+    SERVICE_URL,
     req.body
   );
 
