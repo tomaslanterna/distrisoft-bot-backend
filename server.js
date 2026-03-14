a9dcb24def75dad19430eae8d4a6ab2e78a1efdd
tomaslanterna <tomaslanterna@gmail.com>
2026-03-14 02:44:00 -0300

    fix: add cors config

--- server.js
+++ server.js
@@ -22,7 +22,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
